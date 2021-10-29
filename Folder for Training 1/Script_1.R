45dce6fe5277d8f2a259016b806b28e0e416089b
AdamYuanTian <47461989+AdamYuanTian@users.noreply.github.com>
2021-10-29 16:06:13 -0500

    Add files via upload

---
+++ Folder for Training 1/Script_1.R
@@ -0,0 +1,5 @@
+# Install the first R package
+install.packages("BiocManager")
+
+# Install more packages using BiocManager, which install and manager packages from the Bioconductor project for genomic data analysis
+BiocManager::install("DESeq2")
